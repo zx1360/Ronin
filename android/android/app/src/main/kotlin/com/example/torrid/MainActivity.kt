0964e8ebbd02cb9d55fdc441b40c7ef26dc6120e
zx1360 <3039328600@qq.com>
2026-05-31 16:52:53 +0800

    gallery修复对视频类的证书信任问题.

--- android/android/app/src/main/kotlin/com/example/torrid/MainActivity.kt
+++ android/android/app/src/main/kotlin/com/example/torrid/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.torrid
 
 import io.flutter.embedding.android.FlutterActivity
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.HostnameVerifier
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        // 自签证书场景: 跳过主机名验证 (证书 SAN 为空,
+        // 且服务端 IP 在校园网环境下会变化, 无法固定)
+        HttpsURLConnection.setDefaultHostnameVerifier(
+            HostnameVerifier { _, _ -> true }
+        )
+        super.onCreate(savedInstanceState)
+    }
+}
